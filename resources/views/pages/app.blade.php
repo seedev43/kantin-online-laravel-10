24d027bcabe6311c87a9e412008ce8e54aa1f94b
Ahmad Dzaky <dzakykun11@gmail.com>
2024-04-21 19:09:10 +0700

    add format rupiah

--- resources/views/pages/app.blade.php
+++ resources/views/pages/app.blade.php
@@ -16,7 +16,7 @@
             class="bg-white border-gray-200 dark:bg-gray-900 fixed w-full z-50 top-0 start-0 border-b dark:border-gray-600">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Enak-In</span>
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Makan Cuy</span>
                 </a>
                 <div class="flex lg:ml-[35%] md:order-2">
                     <!-- button search -->
@@ -265,13 +265,13 @@
                         <div class="mt-2 mb-5 flex items-center justify-between">
 
                             <p>
-                                <span class="text-2xl font-bold text-slate-900 dark:text-white">Rp.
-                                    {{ $product->price }}</span>
+                                <span id="price"
+                                    class="text-xl lg:text-2xl font-bold text-slate-900 dark:text-white">{{ $product->price }}</span>
 
                             </p>
                         </div>
                         @if (Auth::check())
-                            <button onclick="tes(this)"
+                            <button onclick="openModal(this)"
                                 class="flex items-center justify-center w-full rounded-md dark:bg-slate-900 bg-slate-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
                                 data-modal-target="crud-modal-{{ $product->id }}"
                                 data-modal-toggle="crud-modal-{{ $product->id }}"
@@ -325,10 +325,10 @@
                             <!-- Modal body -->
                             <form class="p-4 md:p-5">
                                 <div class="grid gap-4 mb-4 grid-cols-2">
-                                    <div class="col-span-2" id="product-{{ $product->id }}">
+                                    <div class="col-span-2">
                                         <label for="quantity-input-{{ $product->id }}"
-                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose
-                                            quantity:</label>
+                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Quantity:</label>
                                         <div class="relative flex items-center w-full">
                                             <button type="button" id="decrement-button"
                                                 data-input-counter-decrement="quantity-input-{{ $product->id }}"
@@ -364,27 +364,29 @@
                                             Note</label>
                                         <textarea id="description" rows="4"
                                             class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                            placeholder="Write product description here"></textarea>
+                                            placeholder="Write order note here"></textarea>
                                     </div>
-                                    <div class="col-span-2 ">
-                                        <label for="price"
-                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-                                        {{-- <input type="hidden" id="productPrice-{{ $product->id }}"
-                                            value="{{ $product->price }}"> --}}
-                                        <input type="number" name="price" id="price-{{ $product->id }}"
+                                    <div class="col-span-2">
+                                        {{-- <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label> --}}
+                                        {{-- <input type="number" name="price" id="price-{{ $product->id }}"
                                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                            value="{{ $product->price }}" readonly>
+                                            value="{{ $product->price }}" readonly> --}}
+                                        <p class="text-lg font-medium text-gray-900 dark:text-white">
+                                            <span id="price-{{ $product->id }}">{{ $product->price }}</span>
+                                        </p>
+                                        <p class="text-sm font-normal text-gray-900 dark:text-white mt-2">
+                                            Stock: {{ $product->stock }}
+                                        </p>
                                     </div>
                                 </div>
                                 <button type="submit" id="submit"
                                     class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                            d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                            clip-rule="evenodd"></path>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                                     </svg>
-                                    Add new product
+                                    Add to cart
                                 </button>
                             </form>
                         </div>
